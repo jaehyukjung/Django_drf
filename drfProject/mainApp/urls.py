d60a41221318b750475f741f7ace02ed08bc6bac
jaehyukjung <jhdool1594@gmail.com>
2022-09-25 22:59:44 +0900

    DB 수정 필요 음성인식으로 토큰 전달까지 완료

--- drfProject/mainApp/urls.py
+++ drfProject/mainApp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import ReviewList, ReviewDetail, CoffeeList, CoffeeDetail
+from .views import ReviewList, ReviewDetail, CoffeeList, CoffeeDetail, OrderList, OrderDetail
 
 urlpatterns = [
     path('review/', ReviewList.as_view()),
     path('review/<int:pk>/', ReviewDetail.as_view()),
     path('coffee/', CoffeeList.as_view()),
     path('coffee/<int:pk>/', CoffeeDetail.as_view()),
+    path('order', OrderList.as_view()),
+    path('coffee/<int:pk>/',  OrderDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- drfProject/mainApp/views.py
+++ drfProject/mainApp/views.py
@@ -9,6 +9,7 @@ from .serializers import ReviewSerializer,CoffeeSerializer
 from .models import Coffee, Review
 
 from django.template import loader
+import my_stt
 
 
 # Create your views here.
@@ -94,3 +95,44 @@ class CoffeeDetail(APIView):
         review.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+class OrderList(APIView):
+    def get(self, request):
+        lst = my_stt.my_fun()
+        
+        reviews = Coffee.objects.filter(name__in= lst)
+        serializer = CoffeeSerializer(reviews, many = True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CoffeeSerializer(
+            data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) # 성공한 경우에는 200포트
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 실패한 경우에는 400포트
+
+class OrderDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Coffee.objects.get(pk = pk)
+        except Coffee.DoesNotExist:
+            raise Http404
+    
+    def get(self, request,pk, format=None):
+        review = self.get_object(pk)
+        serializer = CoffeeSerializer(review)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        review = self.get_object(pk)
+        serializer = CoffeeSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        review = self.get_object(pk)
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
